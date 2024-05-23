d29c5e4c176ffe4320f912e9722974d6f6aea053
Altrono <hbotema16@gmail.com>
2024-05-23 12:09:32 +0200

    Fix fonts error during deployment

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Jost } from 'next/font/google';
+import { Jost, Lusitana } from 'next/font/google';
 
 export const jost = Jost({subsets: ['latin']});
+export const lusitana = Lusitana({weight: ['400', '700'], subsets: ['latin']});
 
