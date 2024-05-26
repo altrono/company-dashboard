3860fc40e4804f5c018501a5ea60ed5ee69f35a6
Altrono <hbotema16@gmail.com>
2024-05-26 02:44:02 +0200

    fix bugs

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Softwaregnies Admin.</strong> Contact Admin{' '}
+            <strong>Welcome to Softwaregnies Dashboard.</strong> {' '}
             
-            for staff only.
+            Staff only.
           </p>
           <Link
             href="/login"
