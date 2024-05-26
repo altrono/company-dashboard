0d2d15156e211374f6f2a459b05f3144f45da91b
Altrono <hbotema16@gmail.com>
2024-05-26 03:29:05 +0200

    Implement  metadata to the application with  Metadata API.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import '@/app/ui/global.css';
 import { jost } from './ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | SoftwareGenies Dashboard',
+    default: 'SoftwareGenies Dashboard'
+  },
+  description: 'Innovate, Analyze, Succeed',
+  metadataBase: new URL('https://company-dashboard-pi.vercel.app/'),
+};
+
+
 
 export default function RootLayout({
   children,
